10895f14933e4da4dc0637834ea461ec42bae472
Daniel Morin <daniel.morin@collabora.com>
2024-08-13 08:58:46 -0400

    analytics: fix includes

--- subprojects/gst-plugins-bad/gst-libs/gst/analytics/analytics.h
+++ subprojects/gst-plugins-bad/gst-libs/gst/analytics/analytics.h
@@ -31,6 +31,7 @@
 #include <gst/analytics/gstanalyticsobjectdetectionmtd.h>
 #include <gst/analytics/gstanalyticsobjecttrackingmtd.h>
 #include <gst/analytics/gsttensor.h>
+#include <gst/analytics/gstanalyticstensormtd.h>
 #include <gst/analytics/gsttensormeta.h>
 
 #endif /* __ANALYTICS_H__ */
